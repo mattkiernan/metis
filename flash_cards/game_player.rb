080e8e9b0b891f1702f4f7669a9728721df1b20d
mattkiernan <matt.kiernan@gmail.com>
2014-09-19 16:14:11 -0400

    cleanup

--- flash_cards/game_player.rb
+++
@@ -1,6 +0,0 @@
-require "./flashcard_game"
-require "./deck_reader"
-
-deck_reader = DeckReader.new("decks.txt")
-flashcard_game = FlashCardGame.new(deck_reader.get_decks)
-flashcard_game.play
--- lockers/quicktest.rb
+++
@@ -1,5 +0,0 @@
-lockers = []
-
-lockers <<(1..100).each {|locker| {locker_number: locker, locker_state: "closed"}}
-
-puts lockers
