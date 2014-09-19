92d7c111d1f2af134e9fb72c24963a7b8e89c981
mattkiernan <matt.kiernan@gmail.com>
2014-09-19 13:02:06 -0400

    Added partially fixed database for jukebox

--- jukebox/database.rb
+++ jukebox/database.rb
@@ -1,20 +1,24 @@
-require "CSV"
+require "csv"
 
 class Database
 
-	def initialize
-		@database = {}
+	def initialize(file_name)
+		@file_name = file_name
+		@artists = {}
 	end
 
 	def populate_database
-		CSV.foreach("music.csv", {headers:true}).map do |row|
-			@database = {row["Artist"] => row["Name"]}
-			user_input = "Taylor Swift"
-			puts @database[user_input]
+		CSV.foreach(@file_name, {headers: true}) do |row|
+			if @artists[row["Artist"]]
+				@artists[row["Artist"]] << row["Name"]
+			else
+				@artists[row["Artist"]] = [row["Name"]]
+			end
 		end
+		puts @artists
 	end
 
 end
 
-database = Database.new
+database = Database.new("music.csv")
 database.populate_database
